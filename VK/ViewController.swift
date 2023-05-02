d9a6bd1b52e65a58aeed827635a0ce40a885136b
Marsalov Sergey <marsalovv@gmail.com>
2023-05-02 12:27:06 +0300

    Initial Commit

---
+++ VK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK
+//
+//  Created by Sergey Marsalov on 02.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
