039538aab5743544382841d6969458d947c47ef2
Marsalov Sergey <marsalovv@gmail.com>
2023-09-05 16:53:29 +0300

    Added ActivityIndicator to FeedTableViewController

--- VK/ViewControllers/FeedTableViewController.swift
+++ VK/ViewControllers/FeedTableViewController.swift
@@ -6,12 +6,24 @@ class FeedTableViewController: UITableViewController {
     
     private  var posts: [NewsModel] = []
     private var nextFrom: String = ""
-
+    
+    private lazy var loadIndicatorView: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView(style: .large)
+        indicator.hidesWhenStopped = true
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        return indicator
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .Pallete.white
         title = ~"feedTableViewController"
+        
+        view.addSubview(loadIndicatorView)
+        loadIndicatorView.centerYAnchor.constraint(equalTo: view.layoutMarginsGuide.centerYAnchor).isActive = true
+        loadIndicatorView.centerXAnchor.constraint(equalTo: view.layoutMarginsGuide.centerXAnchor).isActive = true
         
         tableView.register(PostTableViewCell.self, forCellReuseIdentifier: "postTableViewCell")
         tableView.rowHeight = UITableView.automaticDimension;
@@ -33,6 +45,7 @@ class FeedTableViewController: UITableViewController {
     }
     
     @objc private func getNews() {
+        loadIndicatorView.startAnimating()
         VK.API.NewsFeed.get([.filters : "post", .startFrom : nextFrom, .count : "25"])
             .onSuccess() {newsFeedData in
                 guard let newsFeed = try? JSONDecoder().decode(NewsFeedModel.self, from: newsFeedData) else {return}
@@ -41,6 +54,7 @@ class FeedTableViewController: UITableViewController {
                 
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
+                    self.loadIndicatorView.stopAnimating()
                 }
             }
             .onError({ error in
@@ -60,11 +74,12 @@ class FeedTableViewController: UITableViewController {
         
         cell.postImageView.image = nil
         cell.setupCell(post: posts[indexPath.row])
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if indexPath.row == posts.count - 3 {
+        if indexPath.row >= posts.count - 3 {
             getNews()
         }
     }
